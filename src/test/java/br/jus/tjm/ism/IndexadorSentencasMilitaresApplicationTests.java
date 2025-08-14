0aa1b0f501b331bb003d4b9e2005ee26cfc9a789
Nathan Pierazolli <npierazolli@gmail.com>
2025-08-14 08:59:32 -0300

    first commit

---
+++ src/test/java/br/jus/tjm/ism/IndexadorSentencasMilitaresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.jus.tjm.ism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndexadorSentencasMilitaresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
